89ad4b16bdd3971f93925cea6415d7cabb3a848c
pyfuhr <71835940+pyfuhr@users.noreply.github.com>
2021-03-30 18:19:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import logging
-import json
+import json, os
 
 
 app = Flask(__name__)
@@ -83,4 +83,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port))
